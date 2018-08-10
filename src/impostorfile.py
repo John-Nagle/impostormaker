864aa5b7efdbd1e3d3b75c9b9ade3a3b8a29fd63
John Nagle <nagle@sitetruth.com>
2018-08-09 20:22:35 -0700

    Finding red rectangle

--- src/impostorfile.py
+++ src/impostorfile.py
@@ -16,6 +16,7 @@
 import PIL
 import PIL.Image
 import PIL.ImageStat
+import math
 
 #   Useful functions
 
@@ -32,6 +33,16 @@ def insetrect(rect, inset) :
     if insetrect[0] >= rect[2] or insetrect[2] <= rect[0] or insetrect[1] >= rect[3] or insetrect[3] <= rect[1] :
         return None
     return(insetrect)
+    
+def colorinrange(color, colorrange) :
+    '''
+    Is color within bounds given?
+    '''
+    (locolor, hicolor) = colorrange # color limits
+    for i in range(len(color)) :    # test each color channel
+        if (color[i] < locolor[i]) or (color[i] > hicolor[i]) :
+            return False            # out of bounds
+    return True                     # color 
 
 
 def combinestddev(x, y) :
@@ -86,48 +97,83 @@ class ImpostorFile:
         Raises exception if no reasonable frame
         '''
         pass                                            # ***MORE***
-        
-    def sweeph(self, top, bottom, width) :
+                
+    def sweeph(self, top, bottom, xstart, xend, thickness, colorrange) :
         '''
-        Sweep across the image horizontally in bands and return
-        the standard deviation for each band. 
+        Sweep across the image horizontally in bands of "thickness"
+        and return the most uniform band where the color is in colorrange.
+        
+        if xstart > xend, scan right to left.
         
         The bottom pixel is bottom-1, i.e. top=0, bottom=10 means 0..9
         '''
-        imgrect = self.inputrgb.getbbox()                    # bounds of image
-        return [self._rectuniformity((x, top, x+width, bottom)) for x in range(imgrect[0], imgrect[2]-width, width)]
-            
-        
-    def sweepv(self, left, right, height) :
+        incr = thickness
+        if xstart > xend :                                  # invert incr if right to left
+            incr = -incr
+        beststddev = math.inf                               # best standard deviation seen
+        bestx = None                                        # no hit yet
+        for x in range(xstart,xend, incr) :
+            (counts, means, stddevs) = self._rectuniformity((min(x,x+incr), top, max(x, x+incr), bottom))  # scan a rectangle
+            if not colorinrange(means, colorrange) :        # if color outside range for this scan (anywhere near red)
+                continue                                    # ignore
+            stddev = sum(stddevs) / 3                       # std dev of all 3 colors
+            if stddev < beststddev :                        # new winner
+                bestx = x
+                beststdddev = stddev
+        return bestx                                        # winner, or none
+                
+    def sweepv(self, left, right, ystart, yend, thickness, colorrange) :
         '''
-        Sweep across the image horizontally in bands and return
-        the standard deviation for each band. 
-        '''
-        imgrect = self.inputrgb.getbbox()                    # bounds of image
-        return [self._rectuniformity((left, y, right, y+height)) for y in range(imgrect[1], imgrect[3]-height, height)]
+        Sweep across the image horizontally in bands of "thickness"
+        and return the most uniform band where the color is in colorrange.
         
+        if ystart > yend, scan bottom to top
+        
+        The bottom pixel is bottom-1, i.e. top=0, bottom=10 means 0..9
+        '''
+        incr = thickness
+        if ystart > yend :                                  # invert incr if right to left
+            incr = -incr
+        beststddev = math.inf                               # best standard deviation seen
+        besty = None                                        # no hit yet
+        for y in range(ystart, yend, incr) :
+            (counts, means, stddevs) = self._rectuniformity((left, min(y,y+incr), right, max(y, y+incr)))  # scan a rectangle
+            ####print("Sweepv: ",y,means, rect)                       # ***TEMP***
+            if not colorinrange(means, colorrange) :        # if color outside range for this scan (anywhere near red)
+                continue                                    # ignore
+            stddev = sum(stddevs) / 3                       # std dev of all 3 colors
+            if stddev < beststddev :                        # new winner
+                besty = y
+                beststdddev = stddev
+        return besty                                        # winner, or none
+                
+     
     def testsweeps(self) :
         '''
         Test and dump for sweep. Debug use only.
         '''
+        REDRANGE = ((128,0,0),(255,127,127))                # color range where red dominates
         imgrect = self.inputrgb.getbbox()                   # bounds of image
         (left, top, right, bottom) = imgrect
-        height = bottom-top+1
+        height = bottom - top
+        width = right -left
         scantop = top + int(height/4)                       # scan the middle half of the image
         scanbot = bottom - int(height/4)
-        width = 10                                          # band width 10 pixels
-        print("Test horizontal sweep")
-        scanres = self.sweeph(scantop, scanbot, width)
-        for i in range(len(scanres)) :
-            print(scanres[i])        
-        print("")
-        print("Test vertical sweep")
         scanleft = left + int(width/4)
-        scanright = right -int(width/4)
-        scanres = self.sweepv(scanleft, scanright, width)
-        for i in range(len(scanres)) :
-            print(scanres[i])        
-        print("")
+        scanright = right - int(width/4)
+        thickness = 5                                       # minimum frame width
+        print("Test horizontal sweeps")
+        xleft = self.sweeph(scantop, scanbot, left, int((left+right)/2), thickness, REDRANGE)
+        xright = self.sweeph(scantop, scanbot, right, int((left+right)/2), thickness, REDRANGE)
+        print("X frame limits: ", xleft, xright)
+        print("Test vertical sweeps")
+        ytop = self.sweepv(scanleft, scanright, top, int((top+bottom)/2), thickness, REDRANGE)
+        ybot = self.sweepv(scanleft, scanright, bottom, int((top+bottom)/2), thickness, REDRANGE)
+        print("Y frame limits: ", ytop, ybot)
+        if (xright is not None and xleft is not None and ytop is not None and ybot is not None) :
+            viewrect = (xleft, ytop, xright, ybot)
+            croppedrgb = self.inputrgb.crop(viewrect)           # extract rectangle of interest
+            croppedrgb.show()                                   # ***TEMP***
        
         
     def _frameuniformity(self, rect, insetwidth) :
@@ -174,7 +220,46 @@ class ImpostorFile:
         Run the uniformity test on a rectangle.
         '''
         croppedrgb = self.inputrgb.crop(rect)           # extract rectangle of interest
-        ####croppedrgb.show()                               # ***TEMP***
+        ####croppedrgb.show()                           # ***TEMP***
         stats = PIL.ImageStat.Stat(croppedrgb)          # image statistics
         return(stats.count, stats.mean, stats.stddev)
+        
+    def findframeold(self, sweepsize=0.5, colorhint=None) :
+        '''
+        Find solid color frame around image of interest.
+        
+        Sweep size is the size of the band used to sweep
+        across looking for solid color areas. 0.5 means
+        half the image height or width.
+        
+        If color hint is given, colors closer to it will
+        be preferred.
+        '''
+        #   Phase 1: Sweep horizontally and vertically to find the rough frame bounds
+        (left, top, right, bottom) = self.inputrgb.getbbox()  
+        scanthickness = 5                                   # scan a rect 5 pixels at a time
+        #   Horizontal scan
+        height = bottom-top+1
+        scantop = top + int(height*sweepsize*0.5)           # scan the middle half of the image
+        scanbot = bottom - int(height*sweepsize*0.5)
+        scanh = self.sweeph(scantop, scanbot, width)
+        #   Vertical scan
+        width = right-left+1
+        scanleft = left + int(width*sweepsize*0.5)
+        scanright = right -int(width*sweepsize*0.5)
+        scanh = self.sweepv(scanleft, scanright, width)
+        
+    def findframe(self, rect=None, minthickness=10) :
+        '''
+        Find solid color frame within rect
+        
+        The frame must be at least minthickness thick.
+        '''
+        if rect is None :                                   # if no rect given
+            rect = self.inputrgb.getbbox()                  # use entire image
+        if minthickness < 2 :                               # frame must be at least two pixels
+            return None                                     # not meaningful
+        #   ***MORE***
+
+        
 
